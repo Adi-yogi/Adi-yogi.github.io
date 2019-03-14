f00ae4dd1ddb467552f3e6c7f532247594f17588
Adi-yogi <48556421+Adi-yogi@users.noreply.github.com>
2019-03-14 20:05:55 +0530

    Adding build files

---
+++ precache-manifest.d38b7c14442e71d262de6082e5fa5696.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "825821aeee35790f7760",
+    "url": "/static/js/main.825821ae.chunk.js"
+  },
+  {
+    "revision": "914166feddad5bc1ed21",
+    "url": "/static/js/2.914166fe.chunk.js"
+  },
+  {
+    "revision": "825821aeee35790f7760",
+    "url": "/static/css/main.a23007b3.chunk.css"
+  },
+  {
+    "revision": "660156df6c3c38cfde22c9332bc15fb4",
+    "url": "/index.html"
+  }
+];
